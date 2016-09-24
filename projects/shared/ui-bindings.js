cb4b6fe2442f53f12c8cc01d22834c2c2832a81d
Tony Wallace <tony@tonywallace.com>
2016-09-24 15:56:51 -0400

    Linked number inputs to range inputs for subtractive and FM synthesis projects.

--- projects/shared/ui-bindings.js
+++ projects/shared/ui-bindings.js
@@ -6,15 +6,21 @@ function UIBindings() {
 		element.value = param.value;
 		element.addEventListener('input', function(e) {
 			param.value = e.target.value;
+			_instance.updateValueDisplayForInput(element);
 		});
+
+		_instance.bindValueDisplayForAudioParamInput(element, param);
 	};
 
 	_instance.bindGainParamInput = function(selector, param) {
 		var element = document.querySelector(selector);
 		element.value = param.value;
 		element.addEventListener('input', function(e) {
-			param.value = Math.pow(parseFloat(e.target.value), 0.6);
+			param.value = _instance.calculateGainValue(e.target.value);
+			_instance.updateValueDisplayForInput(element);
 		});
+
+		_instance.bindValueDisplayForGainParamInput(element, param);
 	};
 
 	_instance.bindTypeParamInput = function(selector, param) {
@@ -44,7 +50,11 @@ function UIBindings() {
 			} else {
 				object[property] = value;
 			}
+
+			_instance.updateValueDisplayForInput(element);
 		});
+
+		_instance.bindValueDisplayForNumericPropertyInput(element, object, property);
 	};
 
 	_instance.bindBooleanPropertyInput = function(selector, object, property) {
@@ -53,6 +63,130 @@ function UIBindings() {
 		element.addEventListener('bindTypeParamInput', function(e) {
 			object[property] = e.target.checked;
 		});
+	};
+
+	_instance.bindValueDisplayForAudioParamInput = function(inputElement, param) {
+		var parentElement = inputElement.parentNode;
+		if (parentElement.classList.contains('input-group')) {
+			var valueElement = parentElement.querySelector('.value-display');
+			if (valueElement) {
+				valueElement.value = _instance.clampValueToRangeOfInput(inputElement, param.value);
+
+				valueElement.addEventListener('blur', function(e) {
+					e.target.value = _instance.clampValueToRangeOfInput(inputElement, e.target.value);
+					param.value = e.target.value;
+					_instance.updateInputForValueDisplay(valueElement);
+				});
+
+				valueElement.addEventListener('keyup', function(e) {
+					if (e.which === 13) {
+						e.target.blur();
+					}
+				});
+			}
+		}
+	};
+
+	_instance.bindValueDisplayForGainParamInput = function(inputElement, param) {
+		var parentElement = inputElement.parentNode;
+		if (parentElement.classList.contains('input-group')) {
+			var valueElement = parentElement.querySelector('.value-display');
+			if (valueElement) {
+				valueElement.value = param.value;
+
+				valueElement.addEventListener('blur', function(e) {
+					e.target.value = _instance.clampValueToRangeOfInput(inputElement, e.target.value);
+					param.value = _instance.calculateGainValue(e.target.value);
+					_instance.updateInputForValueDisplay(valueElement);
+				});
+
+				valueElement.addEventListener('keyup', function(e) {
+					if (e.which === 13) {
+						e.target.blur();
+					}
+				});
+			}
+		}
+	};
+
+	_instance.bindValueDisplayForNumericPropertyInput = function(inputElement, object, property) {
+		var parentElement = inputElement.parentNode;
+		if (parentElement.classList.contains('input-group')) {
+			var valueElement = parentElement.querySelector('.value-display');
+			if (valueElement) {
+
+				if (typeof object[property] === 'function') {
+					valueElement.value = _instance.clampValueToRangeOfInput(inputElement, object[property]());
+				} else {
+					valueElement.value = _instance.clampValueToRangeOfInput(inputElement, object[property]);
+				}
+
+				valueElement.addEventListener('blur', function(e) {
+					e.target.value = _instance.clampValueToRangeOfInput(inputElement, parseFloat(e.target.value));
+
+					if (typeof object[property] === 'function') {
+						object[property](e.target.value);
+					} else {
+						object[property] = e.target.value;
+					}
+
+					_instance.updateInputForValueDisplay(valueElement);
+				});
+
+				valueElement.addEventListener('keyup', function(e) {
+					if (e.which === 13) {
+						e.target.blur();
+					}
+				});
+			}
+		}
+	};
+
+	_instance.updateValueDisplayForInput = function(inputElement) {
+		var parentElement = inputElement.parentNode;
+		if (parentElement.classList.contains('input-group')) {
+			var valueElement = parentElement.querySelector('.value-display');
+			if (valueElement) {
+				valueElement.value = _instance.roundValue(inputElement.value);
+			}
+		}
+	};
+
+	_instance.updateInputForValueDisplay = function(valueElement) {
+		var parentElement = valueElement.parentNode;
+		if (parentElement.classList.contains('input-group')) {
+			var inputElement = parentElement.querySelector('input:not(.value-display)');
+			if (inputElement) {
+				inputElement.value = valueElement.value;
+			}
+		}
+	};
+
+	_instance.clampValueToRangeOfInput = function(inputElement, value) {
+		var min = parseFloat(inputElement.getAttribute('min'));
+		var max = parseFloat(inputElement.getAttribute('max'));
+		if (!isNaN(max) && value > max) {
+			return max;
+		} else if (!isNaN(min) && value < min) {
+			return min;
+		} else {
+			return value;
+		}
+	};
+
+	_instance.calculateGainValue = function(value) {
+		if (value > 1.0) {
+			value = 1.0;
+		} else if (value < 0.0) {
+			value = 0.0;
+		} else {
+			value = Math.pow(parseFloat(value), 0.6);
+		}
+		return _instance.roundValue(value);
+	};
+
+	_instance.roundValue = function(value) {
+		return Math.round(value * 100) / 100;
 	};
 
 }
